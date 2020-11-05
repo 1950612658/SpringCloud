cb96b844dfa4a8339a12d35da8af0751c041386b
1950612658 <1950612658@qq.com>
2020-11-05 11:26:05 +0800

    第一次提交

---
+++ git-demo1/src/com/sujie/git/teatMain.java
@@ -0,0 +1,11 @@
+package com.sujie.git;
+
+/**
+ * @Auther : coach
+ * @create : 2020/11/5 0005 11:24
+ */
+public class teatMain {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
