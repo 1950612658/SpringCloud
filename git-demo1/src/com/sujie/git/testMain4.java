17f125464816a7bc0d580c04d4f9f68025543583
1950612658 <1950612658@qq.com>
2020-11-05 11:38:22 +0800

    第四次提交

--- git-demo1/src/com/sujie/git/testMain4.java
+++ git-demo1/src/com/sujie/git/testMain4.java
@@ -5,4 +5,7 @@ package com.sujie.git;
  * @create : 2020/11/5 0005 11:36
  */
 public class testMain4 {
+    public static void main(String[] args) {
+        System.out.println("尝试第四次修改");
+    }
 }
