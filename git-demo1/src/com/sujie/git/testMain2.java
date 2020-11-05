9082c45f1953d341994496a7caa570c3e7e8a7b5
1950612658 <1950612658@qq.com>
2020-11-05 11:29:32 +0800

    第一次提交

---
+++ git-demo1/src/com/sujie/git/testMain2.java
@@ -0,0 +1,11 @@
+package com.sujie.git;
+
+/**
+ * @Auther : coach
+ * @create : 2020/11/5 0005 11:28
+ */
+public class testMain2 {
+    public static void main(String[] args) {
+        System.out.println("尝试第二次修改");
+    }
+}
